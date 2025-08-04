d7dacc9efd48794b3cb9876a34a21db9f291a006
b-hyoung <youqlrqod@gmail.com>
2025-08-04 23:32:54 +0900

    도커셋팅 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,8 +35,3 @@ else:
 @app.get("/api")
 def read_root():
     return {"message" : "FASTAPI 서버가 실행 중입니다."}
-
-@app.get("/ping")
-def ping():
-    print("Ping called from Android App!")
-    return {"message": "pong"}
